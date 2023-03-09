59ec9c89fe98af5234ce1ca09eb374d7e24a2343
soy-chrislo <green13-16l@hotmail.com>
2023-03-09 09:52:02 -0500

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,26 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL){
+  console.log(`Crawling: ${currentURL}`);
+
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399){
+      console.error(`Error fetching URL: ${currentURL}, status: ${resp.status}`);
+      return;
+    }
+    const contentType = resp.headers.get('content-type');
+    if (contentType.includes('text/html')){
+      console.error(`Error fetching URL: ${currentURL}, content-type: ${contentType}, expected: text/html`);
+      return; 
+    }
+
+    console.log(await resp.text());
+  } catch (e) {
+    console.error(`Error fetching URL: ${currentURL}, reason: ${e.message}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL){
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -37,5 +58,6 @@ function normalizeUrl(urlString){
 
 module.exports = {
   normalizeUrl,
-  getURLsFromHTML
+  getURLsFromHTML,
+  crawlPage,
 }
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const { crawlPage } = require('./crawl.js');
+
+
+function main(){
+  checkArguments();
+
+  console.log('Starting craw');
+
+  const baseURL = process.argv[2];
+  console.log(`Base URL: ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+
+function checkArguments(){
+  if (process.argv.length < 3){
+    console.error('No website URL provided');
+    process.exit(1);
+  }
+  if (process.argv.length > 3){
+    console.error('Too many arguments');
+    process.exit(1);
+  }
+}
+
+main();
